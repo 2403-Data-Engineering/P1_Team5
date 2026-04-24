e6727aa5c2f8ce57fd258e1ff7e634714239bd59
Jonathan <jonathanu064@yahoo.com>
2026-04-24 14:29:39 -0500

    excluded tableparquet from git

--- dim_account.py
+++ dim_account.py
@@ -16,7 +16,7 @@ def create_dim_account(driver):
         MATCH (a:Account)
         RETURN a.id,a.risk_score,a.flagged,a.drain_flag,a.fan_in_flag,a.fan_out_flag,a.in_cycle_flag,
         a.guilt_by_association_flag
-        LIMIT 100;
+      
     """
     with driver.session() as session:
         result = session.run(query)
@@ -26,7 +26,7 @@ def create_dim_account(driver):
 
 
 result = create_dim_account(driver)
-result.to_parquet("\TableParquet\dim_account.parquet", index=False)
+result.to_parquet("TableParquet/dim_account.parquet", index=False)
 print(f"Written {len(result)} rows")
 #result = get_transaction_types(driver)
 # for row in result:
