8d7af28f9d305dbd38d6f76f5c5e79496069c62c
Jonathan <jonathanu064@yahoo.com>
2026-04-28 12:51:31 -0500

    finished dim_community

--- CreateParquet/dim_community.py
+++ CreateParquet/dim_community.py
@@ -9,19 +9,17 @@ def create_dim_community():
         RETURN a.community_id AS community_id,
             count(DISTINCT a) AS member_count,
             sum(t.amount) AS total_internal_volume
-
-        LIMIT 100
     """
     with driver.session() as session:
         result = session.run(query)
-        return [record.data() for record in result]
-        # df = pd.DataFrame([record.data() for record in result])
-        # return df
+        #return [record.data() for record in result]
+        df = pd.DataFrame([record.data() for record in result])
+        return df
 
 
-# result = create_dim_account()
-# result.to_parquet("TableParquet/dim_account.parquet", index=False)
-# print(f"Written {len(result)} rows")
 result = create_dim_community()
-for row in result:
-    print(row)
+result.to_parquet("TableParquet/dim_community.parquet", index=False)
+print(f"Written {len(result)} rows")
+# result = create_dim_community()
+# for row in result:
+#     print(row)
